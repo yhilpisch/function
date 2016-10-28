4d90fe98c295e8947276621a799d01ee40122684
yves <yves@tpq.io>
2016-10-28 09:41:51 +0200

    Added type checking.

--- function.py
+++ function.py
@@ -11,6 +11,12 @@ def f(x):
     =======
     y: float
         (positive) output number
+        
+    Raises
+    ======
+    TypeError if x is neither int or float
     '''
+    if type(x) not in [int, float]:
+        raise TypeError('Parameter must be integer or float.')
     y = x ** 2
     return y
