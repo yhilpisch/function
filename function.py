6d3847ed14e946074d9ce8b31ea24c90b64fbf8c
yves <yves@tpq.io>
2016-10-28 09:40:33 +0200

    Initial commit.

---
+++ function.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+def f(x):
+    ''' Function to compute the square of a number.
+    
+    Parameters
+    ==========
+    x: float
+        input number
+    
+    Returns
+    =======
+    y: float
+        (positive) output number
+    '''
+    y = x ** 2
+    return y
